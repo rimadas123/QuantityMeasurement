c1fa82d7ef184758b1ad394dbd9465e01d38f38f
rimadas123 <rdas5969@gmail.com>
2020-04-10 17:38:55 +0530

    Added : to check if temperature equate in fahrenheit and celsius

--- src/test/java/TemperatureTest.java
+++ src/test/java/TemperatureTest.java
@@ -1,2 +1,14 @@
+import com.quantity_measurement.LengthCalculate;
+import com.quantity_measurement.UnitConverter;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TemperatureTest {
+
+    @Test
+    public void given212FahrenheitAnd100CelsiusShouldReturnEqual(){
+        LengthCalculate calculate = new LengthCalculate(UnitConverter.TEMPERATURE.FAHRENHEIT,212.0);
+        LengthCalculate calculate1 = new LengthCalculate(UnitConverter.TEMPERATURE.CELSIUS,100.0);
+        Assert.assertEquals(calculate,calculate1);
+    }
 }
