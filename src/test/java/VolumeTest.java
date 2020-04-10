ae0608868137eb5a5774764a211c8845fc2e779a
rimadas123 <rdas5969@gmail.com>
2020-04-10 14:01:49 +0530

    Added : to check if gallon is equal to litres and litre is equal to mililitres

--- src/test/java/VolumeTest.java
+++ src/test/java/VolumeTest.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import com.quantity_measurement.LengthCalculate;
+import com.quantity_measurement.UnitConverter;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class VolumeTest {
+    @Test
+    public void given1GallonAnd3LitresShouldReturnEqual(){
+        LengthCalculate calculate = new LengthCalculate(UnitConverter.VOlUME.GALLON,1.0);
+        LengthCalculate calculate1 = new LengthCalculate(UnitConverter.VOlUME.LITERS,3.78);
+        Assert.assertEquals(calculate,calculate1);
+    }
+
+    @Test
+    public void given1LitreAnd1000MLShouldReturnEqual(){
+        LengthCalculate calculate = new LengthCalculate(UnitConverter.VOlUME.LITERS,1.0);
+        LengthCalculate calculate1 = new LengthCalculate(UnitConverter.VOlUME.MILLILITERS,1000.0);
+        Assert.assertEquals(calculate,calculate1);
+    }
 }
