eb22f8ceb8852bee020c011c348366ac6e2674aa
rimadas123 <rdas5969@gmail.com>
2020-04-10 14:54:10 +0530

    Added : to check if volumes add in litres

--- src/test/java/VolumeTest.java
+++ src/test/java/VolumeTest.java
@@ -17,4 +17,20 @@ public class VolumeTest {
         LengthCalculate calculate1 = new LengthCalculate(UnitConverter.VOlUME.MILLILITERS,1000.0);
         Assert.assertEquals(calculate,calculate1);
     }
+
+    @Test
+    public void given1GallonAnd3LitresShouldReturn7Litres(){
+        LengthCalculate calculate = new LengthCalculate(UnitConverter.VOlUME.GALLON,1.0);
+        LengthCalculate calculate1 = new LengthCalculate(UnitConverter.VOlUME.LITERS,3.78);
+        double add = calculate.addMethod(calculate,calculate1, UnitConverter.VOlUME.LITERS.unitConversion);
+        Assert.assertEquals(7.56,add,0.0);
+    }
+
+    @Test
+    public void given1LitreAnd1000MLShouldReturn2Litres(){
+        LengthCalculate calculate = new LengthCalculate(UnitConverter.VOlUME.LITERS,1.0);
+        LengthCalculate calculate1 = new LengthCalculate(UnitConverter.VOlUME.MILLILITERS,1000.0);
+        double add = calculate.addMethod(calculate,calculate1, UnitConverter.VOlUME.LITERS.unitConversion);
+        Assert.assertEquals(2,add,0.0);
+    }
 }
